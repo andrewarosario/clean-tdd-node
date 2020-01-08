829e2fcc4b42fc494b3313477bed6ccd8c0c65b1
Andrew Rosário <andrew.arosario@gmail.com>
2020-01-07 22:49:57 -0300

    garante que LoginRouter vai retornar 400 se nao houver email

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,21 @@
+class LoginRouter {
+  route (httpResponse) {
+    if (!httpResponse.body.email) {
+      return { statusCode: 400 }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
